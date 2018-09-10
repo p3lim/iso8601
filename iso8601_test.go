d907e7de86060f2fb270e5524b3c9205f2190912
Adrian L Lange <mail@p3lim.net>
2018-09-10 21:01:46 +0200

    Fix tests

--- iso8601_test.go
+++ iso8601_test.go
@@ -1,7 +1,6 @@
 package iso8601
 
 import (
-	"fmt"
 	"time"
 	"testing"
 )
@@ -9,7 +8,8 @@ import (
 func TestISO8601Format(t *testing.T) {
 	var d time.Duration = 54321 * time.Hour
 
-	if Format(d) != "P6Y2M1W4D" {
-		t.Fatalf("String format for %s does not equal expected %s", d, )
+	expected := "P6Y2M1W4D"
+	if Format(d) != expected {
+		t.Fatalf("String format for %s does not equal expected %s", d, expected)
 	}
 }
