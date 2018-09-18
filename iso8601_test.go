5d8a80466adf2e2c7b68daef312e1b5d5945eaf2
Adrian L Lange <mail@p3lim.net>
2018-09-18 15:15:58 +0200

    More tests and more explicit output

--- iso8601_test.go
+++ iso8601_test.go
@@ -1,15 +1,21 @@
 package iso8601
 
 import (
-	"time"
 	"testing"
+	"time"
 )
 
 func TestISO8601Format(t *testing.T) {
-	var d time.Duration = 54321 * time.Hour
+	tests := map[time.Duration]string{
+		54321 * time.Hour:   "P6Y2M1W4D",
+		54321 * time.Minute: "P1M1WT6H51M",
+		54321 * time.Second: "PT15H5M21S",
+	}
 
-	expected := "P6Y2M1W4D"
-	if Format(d) != expected {
-		t.Fatalf("String format for %s does not equal expected %s", d, expected)
+	for input, expected := range tests {
+		output := Format(input)
+		if output != expected {
+			t.Fatalf("String format for %s does not equal expected %s, got %s", input, expected, output)
+		}
 	}
 }
